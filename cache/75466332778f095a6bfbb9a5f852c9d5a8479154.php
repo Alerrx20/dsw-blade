70396691db791bb8702713d467859d19c2570b45
Alerrx20 <alejandro.real.ramos@gmail.com>
2023-01-11 18:17:09 +0100

    platilla basica

---
+++ cache/75466332778f095a6bfbb9a5f852c9d5a8479154.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Plantilla basica</title>
+</head>
+<body>
+  <h1>Plantilla basica</h1>
+  Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt voluptate nemo obcaecati magnam autem id possimus corporis sed maxime, in corrupti. Maiores illum quo, aspernatur officiis laudantium id dolorum cumque!
+</body>
+</html>
